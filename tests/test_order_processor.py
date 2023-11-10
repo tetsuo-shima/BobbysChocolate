216d09e442411e60c7a906f1317ea87d7520d8fb
Dwight Francis <dwight.l.francis@gmail.com>
2023-11-10 16:10:11 -0500

    test for change_order_sheet

--- tests/test_order_processor.py
+++ tests/test_order_processor.py
@@ -6,6 +6,17 @@ from order_processor import ChocolateType, OrderProcessor
 
 
 class TestOrderProcessor(unittest.TestCase):
+    def test_create_order_sheet(self):
+        class Flavors(Enum):
+            apple = 1
+            grape = 2
+            cherry = 3
+
+        expected = {'apple': 0, 'grape': 0, 'cherry': 0}
+
+        self.assertEqual(OrderProcessor.create_order_sheet(Flavors), expected)
+
+
     def test_process_order_int(self):
         order = {'type': 'milk', 'cash': 12, 'price': 2, 'ratio': 5}
         expected = {'milk': 6, 'violet': 0, 'espresso': 0}
@@ -16,9 +27,14 @@ class TestOrderProcessor(unittest.TestCase):
         expected = {'milk': 6, 'violet': 0, 'espresso': 0}
         self.assertEqual(OrderProcessor.process_order(order), expected)
 
-    def test_process_bonus(self):
+    def test_process_bonus_milk(self):
         order = {'type': 'milk', 'cash': 12.5, 'price': 2.0, 'ratio': 5.0}
         expected = 1
+        self.assertEqual(OrderProcessor.process_bonus(order), expected)
+
+    def test_process_bonus_violet(self):
+        order = {'type': 'violet', 'cash': 13, 'price': 4, 'ratio': 1}
+        expected = 3
         self.assertEqual(OrderProcessor.process_bonus(order), expected)
 
 
