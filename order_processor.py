b01402319085cb6814ffc04d64dec2638d91bfb1
Dwight Francis <dwight.l.francis@gmail.com>
2023-11-11 22:00:52 -0500

    Changed order processor from static object to module; didn't want to obsess about OOP.

--- order_processor.py
+++ order_processor.py
@@ -1,33 +1,25 @@
-from enum import Enum
+from chocolate_type import ChocolateType
 
 
-class ChocolateType(Enum):
-    milk = 1
-    violet = 2
-    espresso = 3
+def create_order_sheet(flavors=ChocolateType) -> dict:
+    return {flavor.name: 0 for flavor in flavors}
 
 
-class OrderProcessor:
-    @staticmethod
-    def create_order_sheet(flavors=ChocolateType) -> dict:
-        return {flavor.name: 0 for flavor in flavors}
+def process_order(order: dict, bonus: bool = False) -> dict:
+    order_sheet = create_order_sheet()
 
-    @staticmethod
-    def process_order(order: dict, bonus: bool = False) -> dict:
-        order_sheet = OrderProcessor.create_order_sheet()
+    for key in order_sheet.keys():
+        if key == order['type']:
+            order_sheet[key] = _calculate_quantity(order)
+            if bonus:
+                order_sheet[key] += process_bonus(order)
 
-        for key in order_sheet.keys():
-            if key == order['type']:
-                order_sheet[key] = OrderProcessor._calculate_quantity(order)
-                if bonus:
-                    order_sheet[key] += OrderProcessor.process_bonus(order)
+    return order_sheet
 
-        return order_sheet
 
-    @staticmethod
-    def _calculate_quantity(order) -> int:
-        return order['cash'] // order['price']
+def _calculate_quantity(order) -> int:
+    return order['cash'] // order['price']
 
-    @staticmethod
-    def process_bonus(order) -> int:
-        return OrderProcessor._calculate_quantity(order) // order['ratio']
+
+def process_bonus(order) -> int:
+    return _calculate_quantity(order) // order['ratio']
