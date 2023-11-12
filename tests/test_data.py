19e1f4c516dedcaede1d0883256db8ffa13488f6
Dwight Francis <dwight.l.francis@gmail.com>
2023-11-12 11:27:48 -0500

    updates in file due to project reorganization

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,12 +1,12 @@
 import unittest
-from data_object import Data
+from data import Data
 
-filename = 'test_orders.csv'
+filename = 'test_resources/test_orders.csv'
 
 
 class TestData(unittest.TestCase):
     def test_read_file(self):
-        orders = Data.read_file(filename)
+        orders = Data.load_orders(filename)
         first = {'type': 'milk', 'cash': 12.1, 'price': 2, 'ratio': 5}
         second = {'type': 'violet', 'cash': 13, 'price': 4, 'ratio': 1}
         third = {'type': 'espresso', 'cash': 6, 'price': 2, 'ratio': 2}
