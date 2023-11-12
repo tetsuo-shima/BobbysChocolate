c0f25653cb7e1206fb3f9a5e0ddc3bf538461070
Dwight Francis <dwight.l.francis@gmail.com>
2023-11-11 22:01:40 -0500

    updated tests after change of order processor from static object to module

--- tests/test_order_processor.py
+++ tests/test_order_processor.py
@@ -1,7 +1,7 @@
 import unittest
 from enum import Enum
 
-from order_processor import OrderProcessor
+import order_processor
 
 
 class TestOrderProcessor(unittest.TestCase):
@@ -13,40 +13,40 @@ class TestOrderProcessor(unittest.TestCase):
 
         expected = {'apple': 0, 'grape': 0, 'cherry': 0}
 
-        self.assertEqual(OrderProcessor.create_order_sheet(Flavors), expected)
+        self.assertEqual(order_processor.create_order_sheet(Flavors), expected)
 
 
     def test_process_order_int(self):
         order = {'type': 'milk', 'cash': 12, 'price': 2, 'ratio': 5}
         expected = {'milk': 6, 'violet': 0, 'espresso': 0}
-        self.assertEqual(OrderProcessor.process_order(order), expected)
+        self.assertEqual(order_processor.process_order(order), expected)
 
     def test_process_order_int_bonus(self):
         order = {'type': 'milk', 'cash': 12, 'price': 2, 'ratio': 5}
         expected = {'milk': 7, 'violet': 0, 'espresso': 0}
-        self.assertEqual(OrderProcessor.process_order(order, True),
+        self.assertEqual(order_processor.process_order(order, True),
                          expected)
 
     def test_process_order_float_bonus(self):
         order = {'type': 'violet', 'cash': 13.4, 'price': 4, 'ratio': 1}
         expected = {'milk': 0, 'violet': 6, 'espresso': 0}
-        self.assertEqual(OrderProcessor.process_order(order, True),
+        self.assertEqual(order_processor.process_order(order, True),
                          expected)
 
     def test_process_order_float(self):
         order = {'type': 'milk', 'cash': 12.5, 'price': 2.0, 'ratio': 5.0}
         expected = {'milk': 6, 'violet': 0, 'espresso': 0}
-        self.assertEqual(OrderProcessor.process_order(order), expected)
+        self.assertEqual(order_processor.process_order(order), expected)
 
     def test_process_bonus_milk(self):
         order = {'type': 'milk', 'cash': 12.5, 'price': 2.0, 'ratio': 5.0}
         expected = 1
-        self.assertEqual(OrderProcessor.process_bonus(order), expected)
+        self.assertEqual(order_processor.process_bonus(order), expected)
 
     def test_process_bonus_violet(self):
         order = {'type': 'violet', 'cash': 13, 'price': 4, 'ratio': 1}
         expected = 3
-        self.assertEqual(OrderProcessor.process_bonus(order), expected)
+        self.assertEqual(order_processor.process_bonus(order), expected)
 
 
 
