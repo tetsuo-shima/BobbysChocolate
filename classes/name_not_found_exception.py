91bccd8c605fce17e66bf77b2f5b0f841944f710
Dwight Francis <dwight.l.francis@gmail.com>
2023-11-15 13:23:58 -0500

    added comment for exception

--- classes/name_not_found_exception.py
+++ classes/name_not_found_exception.py
@@ -1,4 +1,6 @@
 class NameNotFoundException(Exception):
+    """Triggered when a flavor of chocolate isn't found in the chocolate
+    flavor enum"""
     def __init__(self, message='Flavor not found in list of available flavors'):
         self.message = message
         super().__init__(self.message)
