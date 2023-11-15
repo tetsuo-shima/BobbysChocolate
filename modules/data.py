2c83542e691db27991adaa3405e374edbcc63950
Dwight Francis <dwight.l.francis@gmail.com>
2023-11-15 12:53:02 -0500

    wrote type hints added function comments removed copy() in _cast_integers()

--- modules/data.py
+++ modules/data.py
@@ -1,25 +1,36 @@
 import copy
-import csv
+from csv import DictReader
 from resources.constants import ORDER_FILE
+from typing import List, Dict
 
 
-def _read_csv(datafile=ORDER_FILE):
+def _read_csv(datafile: str = ORDER_FILE) -> List[dict]:
     with open(datafile, 'r') as file:
-        return [order for order in csv.DictReader(file)]
+        orders = [order for order in DictReader(file)]
+    return orders
 
 
-def _cast_integers(orders: list):
-    new_orders = copy.deepcopy(orders)
-    for order in new_orders:
+def _cast_integers(orders: List[dict]) -> List[Dict]:
+    """
+    :param orders: list of dictionaries read from CSV. Given 'key: value',
+    all values read by DictReader as strings e.g. '1'
+    :return: list of dictionaries in which values are cast integers when
+    appropriate
+    """
+    for order in orders:
         for key, value in order.items():
             try:
                 order[key] = int(value)
             except ValueError:
                 pass
 
-    return new_orders
+    return orders
 
 
-def load_orders(datafile=ORDER_FILE):
+def load_orders(datafile: str = ORDER_FILE) -> List[dict]:
+    """
+    :param datafile: path to file to be read
+    :return: list of dictionaries representing lines in file
+    """
     orders = _read_csv(datafile)
     return _cast_integers(orders)
